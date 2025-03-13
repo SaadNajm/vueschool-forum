1c72f150e6774deb3326fce397683c32cd2456e9
Saad <saadnajm.msc@gmail.com>
2025-03-13 15:44:26 +0000

    added first component and data

---
+++ src/components/PageHome.vue
@@ -0,0 +1,22 @@
+<template>
+   <div>Hello from page home</div> 
+   <div v-for="thread in threads" :key="thread.id">
+    <h2>{{thread.title}}</h2>
+    <div>
+        
+    </div>
+   </div>
+</template>
+<script>
+import sourceData from '@/data.json'
+export default {
+    data(){
+        return{
+            threads:sourceData.threads
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
